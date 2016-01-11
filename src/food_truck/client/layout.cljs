2d29fa44575571d5e986c1d9dd1516c89ae7c90b
sto <Son.C.To@gmail.com>
2016-01-11 15:09:26 -0500

    add translate-y and translate functions

--- src/food_truck/client/layout.cljs
+++ src/food_truck/client/layout.cljs
@@ -5,13 +5,22 @@
 (defonce div-type (type (js/document.createElement "div")))
 
 (defmulti position #(type %))
-(defmethod position str-type [element-id x y z]
+(defmethod position str-type [element-id x y]
   element-id)
 
 (defn translate-x [x]
   [[1 0 x]
    [0 1 0]
    [0 0 1]])
+
+(defn translate-y [y]
+  [[1 0 0]
+   [0 1 y]
+   [0 0 1]])
+
+(defn translate [x y]
+  (matrix/multiply (translate-x x)
+                   (translate-y y)))
 
 (defn to-css-matrix [m]
   (str "matrix(" (clojure.string/join "," (interleave (first m) (second m))) ")"))
