e87b5a7fa4d80d0a94d24c40eaa20f64586ed496
sto <Son.C.To@gmail.com>
2016-01-12 19:58:54 -0500

    tweenie

--- src/food_truck/client/tweenie.cljs
+++ src/food_truck/client/tweenie.cljs
@@ -5,19 +5,43 @@
 (defn tween [config-map]
   (let [start-val (:from config-map)
         end-val (:to config-map)
-        current-val (atom start-val)
+        duration (:duration config-map)
         step-fn (:step-fn config-map)
-        on-update (:on-update config-map)]
-    (loop [val start-val]
-      (on-update val)
-      (if (< val end-val)
-        (recur (step-fn val))))))
+        on-update (:on-update config-map)
+        
+        current-val (atom start-val)
+        start-time (atom nil)
+        tick-time (atom nil)]
+    (fn []
+      (let [now (js/Date.now) ]
+        (if (nil? @start-time)
+          (reset! start-time now))        
+        (reset! tick-time now))
+
+      (if (and (< (- @tick-time @start-time)
+                  duration)
+               (< @current-val end-val))
+        (let [next-val (step-fn @current-val)]
+          (println "duration " (- @tick-time @start-time)) 
+          (on-update @current-val)
+          (reset! current-val next-val))
+        end-val))))
+
+(defn animate [animation-fn]
+  ((fn animation-loop [time]
+     (animation-fn time)
+     (js/requestAnimationFrame animation-loop))))
 
 
-(defn bar []
-  (tween {:from 1 :to 100
-          :duration 1000
-          :step-fn (fn [val] (inc val))
-          :on-update (fn [val]
-                       (println val)
-                       )}))
+
+(def t (tween {:from 1 :to 100
+               :duration 10000
+               :step-fn (fn [val]
+                          (inc val))
+               :on-update (fn [val]
+                            (println val)
+                            )}))
+
+(defn doit []
+  (animate (fn [time]
+             (t))))
