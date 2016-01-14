584a7ae4d3a56edee37d40643408daf0b73dba39
sto <Son.C.To@gmail.com>
2016-01-13 21:02:09 -0500

    easeout

--- src/food_truck/client/tweenie.cljs
+++ src/food_truck/client/tweenie.cljs
@@ -10,15 +10,20 @@
       delta
       (* fraction delta))))
 
-(defn linear-easing [start-val end-val duration t]
+(defn ease-linear [start-val end-val duration t]
   (let [fraction-of-time (/ t duration)]
     (+ start-val (additional-distance start-val end-val fraction-of-time))))
 
 (defn ease-in [start-val end-val duration t]
   (let [fraction-of-time (/ t duration)
-        fraction-of-time (js/Math.pow fraction-of-time 2)]
-    (+ start-val (additional-distance start-val end-val fraction-of-time)))
-  )
+        fraction-of-time (js/Math.pow fraction-of-time 5)]
+    (+ start-val (additional-distance start-val end-val fraction-of-time))))
+
+(defn ease-out [start-val end-val duration t]
+  (let [fraction-of-time (/ t duration)
+        fraction-of-time (- 1
+                            (js/Math.pow (- 1 fraction-of-time) 5))]
+    (+ start-val (additional-distance start-val end-val fraction-of-time))))
 
 (defn tween [config-map]
   (let [start-val (:from config-map)
@@ -27,24 +32,24 @@
         easing-fn (:easing-fn config-map)
         on-update (:on-update config-map)
         start-time (atom nil)]
-    (fn [time]
+    (fn [clock-time]
       (if (nil? @start-time)
-        (reset! start-time time))
-      (let [time-from-start-time (- time @start-time)
+        (reset! start-time clock-time))
+      (let [time-from-start-time (- clock-time @start-time)
             val (easing-fn start-val end-val duration time-from-start-time)]
         (if (<= time-from-start-time duration)
           (on-update val))
         val))))
 
 (defn animate [tween-fn]
-  ((fn animation-loop [time]
-     (tween-fn time)
+  ((fn animation-loop [clock-time]
+     (tween-fn clock-time)
      (js/requestAnimationFrame animation-loop)
      )))
 
 (def t (tween {:from      1 :to 1000
                :duration  1000
-               :easing-fn ease-in
+               :easing-fn ease-out
                :on-update (fn [val]
                             (let [s (dom/by-id "category-Sandwiches")]
                               (layout/position s val 0))
